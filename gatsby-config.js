a4d582296285c516131f4b0d5d5dad5ef381b7ec
Angelica Reyes <areyes10@student.fullsail.edu>
2025-07-17 19:59:22 -0400

    update to codes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  pathPrefix: "/Angelica_Reyes_DOW-App", // 👈 This is required for GitHub Pages!
   siteMetadata: {
     title: "Angelica's Blog",
-    siteUrl: "https://reyesangelica-fs.github.io/Angelica_Reyes_DOW-App", 
+    siteUrl: "https://reyesangelica-fs.github.io/Angelica_Reyes_DOW-App",
   },
   plugins: [
     "gatsby-plugin-mdx",
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,29 +1,37 @@
-import * as React from "react";
-import { graphql } from "gatsby";
-import { MDXRenderer } from "@mdx-js/react";
+import * as React from "react"
+import { graphql } from "gatsby"
+import { MDXProvider } from "@mdx-js/react"
+import { mdx } from "@mdx-js/react"
+import { useMDXComponent } from "gatsby-plugin-mdx"
 
 const PostTemplate = ({ data }) => {
-    const { frontmatter, body } = data.mdx;
+    const { frontmatter, body } = data.mdx
+    const Content = useMDXComponent(body)
 
     return (
         <main style={{ padding: "2rem", fontFamily: "Arial, sans-serif", maxWidth: "800px", margin: "0 auto" }}>
             <h1>{frontmatter.title}</h1>
-            <p>{frontmatter.date}</p>
-            <MDXRenderer>{body}</MDXRenderer>
+            <p style={{ fontStyle: "italic", color: "#555" }}>{frontmatter.date}</p>
+            <hr />
+            <article>
+                <MDXProvider>
+                <Content />
+                </MDXProvider>
+            </article>
         </main>
-    );
-};
+    )
+}
 
 export const query = graphql`
     query BlogPostById($id: String!) {
         mdx(id: { eq: $id }) {
-        body
+            body
             frontmatter {
                 title
                 date(formatString: "MMMM DD, YYYY")
             }
         }
     }
-`;
+`
 
-export default PostTemplate;
+export default PostTemplate
