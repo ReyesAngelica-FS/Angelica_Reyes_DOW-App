438d99f4fab10434d7926947e5ce4b1a96ed5075
Angelica Reyes <areyes10@student.fullsail.edu>
2025-07-17 17:53:05 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,26 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Demo2`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Angelica Reyes DOW App`,
+    siteUrl: `https://www.yourdomain.tld`, // Replace with your actual URL when deployed
   },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+      __key: "posts",
+    },
+  ],
 };
